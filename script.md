a41390dd2db3111d5f3ab7c6d0890727b39673e0
dimasemenchenko <gararararara001@gmail.com>
2019-02-23 10:29:54 +0200

    Initial commit

---
+++ script.md
@@ -0,0 +1,1 @@
+mvn archetype:generate - generate new maven project
---
+++ src/main/java/ua/com/danit/App.java
@@ -0,0 +1,7 @@
+package ua.com.danit;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
